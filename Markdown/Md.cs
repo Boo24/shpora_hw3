fe87b61e31bf807c68a58e36cfa9e0b00b9056ab
Tatiana Gladysheva <bububu241@gmail.com>
2017-11-21 04:03:11 +0500

    + Md.Render()

--- Markdown/Md.cs
+++ Markdown/Md.cs
@@ -6,10 +6,24 @@ namespace Markdown
 {
 	public class Md
 	{
+	    private Syntax syntax;
+	    private StringAnalyzer analyzer;
+	    public Md()
+	    {
+	        syntax = new Syntax();
+	        syntax.Register("[^_](_)[^_]", "[^_](_)[^_]", "em");
+	        syntax.Register("\\s(__)\\S", "\\S(__)\\s", "strong").With("em");
+	        analyzer = new StringAnalyzer(syntax);
+        }
+
+	    public string Render(string markdownStr)
+	    {
+	        var syntaxParts = analyzer.Analyze(markdownStr);
+	        var tree = new AbstractSyntaxTree();
+	        tree.Build(syntaxParts, markdownStr);
+	        var converter = new Converter(tree);
+	        return converter.Convert();
+	    }
 	}
 
-	[TestFixture]
-	public class Md_ShouldRender
-	{
-	}
 }
